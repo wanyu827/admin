8410f6da83b66d3227f0e1eef64da14b12cf6407
wanyu827 <1968919313@qq.com>
2022-07-21 22:10:42 +0800

    修复bug

--- src/views/RoleList/components/RoleList.vue
+++ src/views/RoleList/components/RoleList.vue
@@ -139,7 +139,7 @@ export default {
     // 删除角色
     sureDelRole (index, row) {
       // console.log(index, row)
-      this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+      this.$confirm('此操作将永久删除该角色, 是否继续?', '提示', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
         type: 'warning'
